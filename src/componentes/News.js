7b91d92d7e39837bd84b84d7d3ce6f878e52084c
Sufiyanweb31 <sufiyanmmc67@gmail.com>
2022-10-03 19:45:44 +0500

    maommon changes

--- src/componentes/News.js
+++ src/componentes/News.js
@@ -4,7 +4,8 @@ import Spiner from './Spiner';
 import PropTypes from 'prop-types'
 import InfiniteScroll from 'react-infinite-scroll-component';
 
-const News = (props) => {
+const News = React.memo((props) => {
+    console.log("ready")
     const [articles, setArticles] = useState([]);
     const [loading, setLoading] = useState(true);
     const [page, setPage] = useState(1);
@@ -14,39 +15,37 @@ const News = (props) => {
         return word[0].toUpperCase() + word.substring(1).toLowerCase();
     }
 
+        useEffect(() => {
+        updateNews();
+       document.title = `${toTitleCase(props.category)} | Top Headlines`;
+       
+    }, []);
+
+    console.log(props.category)
+
+
     const updateNews = async ()=> {
         props.setProgress(0);
-        // const url = `https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apiKey=${props.apikey}&page=${page}&pageSize=${props.pageSize}`;
-
-        const url = await fetch('https://free-news.p.rapidapi.com/v1/search?q=Business&lang=en&page=1&page_size=15', {
+        const respone = await fetch(`https://free-news.p.rapidapi.com/v1/search?q=${props.category}&lang=en&page=1&page_size=15`, {
             method: 'GET',
             headers: {
                 'X-RapidAPI-Key': 'fbb2ea18dbmsh78e40cf6bb77620p1f3b5djsn040bc7202c51',
                 'X-RapidAPI-Host': 'free-news.p.rapidapi.com'
             }
         })
-        let data = await fetch(url);
+        //let data = await fetch(url);
        
         props.setProgress(30);
-        let parsedDate = await data.json();
-        console.log(data)
+        let parsedDate = await respone.json();
+        console.log(parsedDate)
         props.setProgress(70);
        setArticles(parsedDate.articles)
-       setTotalResults(parsedDate.totalResults)
+       // setTotalResults(parsedDate.totalResults)
         setLoading(false)
         props.setProgress(100);
-    }
-
-
-    useEffect(() => {
-        updateNews();
-        document.title = `${toTitleCase(props.category)} | Top Headlines`
         setPage(page+1)
-    }, []);
-
-    
-
-    
+       
+    }
 
    
 
@@ -54,15 +53,7 @@ const News = (props) => {
 
     const fetchMoreData = async () => {
         // const url = `https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apiKey=${props.apikey}&page=${page}&pageSize=${props.pageSize}`;
-        // const url =  await fetch('https://free-news.p.rapidapi.com/v1/search?q=Businessq=Business&lang=en&page=1&page_size=15', options)
-        // const options = {
-        //     method: 'GET',
-        //     headers: {
-        //         'X-RapidAPI-Key': 'fbb2ea18dbmsh78e40cf6bb77620p1f3b5djsn040bc7202c51',
-        //         'X-RapidAPI-Host': 'free-news.p.rapidapi.com'
-        //     }
-        // };
-        const url = await fetch('https://free-news.p.rapidapi.com/v1/search?q=Business&lang=en&page=1&page_size=15', {
+        const respone = await fetch(`https://free-news.p.rapidapi.com/v1/search?q=${props.category}&lang=en&page=${page}&page_size=15`, {
             method: 'GET',
             headers: {
                 'X-RapidAPI-Key': 'fbb2ea18dbmsh78e40cf6bb77620p1f3b5djsn040bc7202c51',
@@ -70,8 +61,8 @@ const News = (props) => {
             }
         })
 
-        let data = await fetch(url);
-        let parsedDate = await data.json();
+        //let data = await fetch(url);
+        let parsedDate = await respone.json();
         setPage(page+1)
         setArticles(articles.concat(parsedDate.articles))
         setTotalResults(parsedDate.totalResults)
@@ -80,11 +71,12 @@ const News = (props) => {
 
 
         return (
-            <div>
+            <>
                 <div className="mb-5">
                     <div className='container'>
                         <h2 className='main-head'> {toTitleCase(props.category)} Top Headlines</h2>
                     </div>
+                    {console.log(articles)}
                     {loading && <Spiner />}
                     <InfiniteScroll
                         dataLength={articles.length}
@@ -101,11 +93,11 @@ const News = (props) => {
                                 })}
                             </div>
                             </div>
-                    </InfiniteScroll>
+                    </InfiniteScroll>  
                 </div>
-            </div>
+            </>
         )
-}
+});
 
 News.defaultProps = {
     country: 'us',
